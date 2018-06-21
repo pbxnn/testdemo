149472bd6d59e296ede3e8e37cc7aa82ca718e25
zxnnn <zxn920628@163.com>
2018-06-22 00:21:59 +0800

    tree depth

--- tree.php
+++ tree.php
@@ -1,15 +1,50 @@
 <?php
 
 class TreeNode {
-    public val = null;
-    public lchild = null;
-    public rchild = null;
+    public $val;
+    public $lchild = NULL;
+    public $rchild = NULL;
 
-    public function __construct($val = null)
+    public function __construct($val)
     {
         $this->val = $val;
     }
 }
 
 
+/**
+ * 求二叉树深度，非递归，广度优先遍历
+ */
+function getDepth($pRoot)
+{
+    if ($pRoot == NULL) {
+        return false;
+    }
+
+    $count = 0;
+    $nextCount = 1;
+    $depth = 0;
+
+    $queue = [];
+    array_push($queue, $pRoot);
+
+    while (!empty($queue)) {
+        $count++;
+        $val = array_shift($queue);
+        if ($val->left) {
+            array_push($queue, $val->left);
+        }
+        
+        if ($val->right) {
+            array_push($queue, $val->right);
+        }
+
+        if ($count == $nextCount) {
+            $depth++;
+            $nextCount = count($queue);
+            $count = 0;
+        }
+    }
+    return $depth;
+}
 
